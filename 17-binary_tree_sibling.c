16b6506f997f6606a75e708dffcd3fb19ebcdd91
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-01 21:07:28 +0000

    returns sibling of given node

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - gets sibling node
+ * @node: pointer to node
+ * Return: sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
