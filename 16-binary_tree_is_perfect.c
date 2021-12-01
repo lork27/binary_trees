1030b68762b8464ce65e436ba286379942a59591
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-01 18:30:26 +0000

    checks if given binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,6 +12,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 			return (1);
+		else
+			return (0);
 	}
 	return (0);
 }
