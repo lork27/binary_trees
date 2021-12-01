077be134babd800eacc9af289472feb1290005d8
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-01 17:13:17 +0000

    task 14 fixed, helper function edgecase was was wrong since the main function filtered out that case already

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 #include <stdio.h>
-int tree_height(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
 /**
  * binary_tree_balance - finds balance of a binary tree
  * @tree: pointer to the root node
@@ -8,27 +8,24 @@ int tree_height(const binary_tree_t *tree);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (0);
+}
+/**
+ * binary_tree_height - measures height of BT
+ * @tree: pointer to the root node
+ * Return: size of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lh;
+	size_t rh;
+
 	if (!tree)
 		return (0);
-	return (tree_height(tree->right) - tree_height(tree->left));
-}
-
-/**
- * tree_height - finds height of branch
- * @tree: pointer to the root node
- * Return: height
- */
-int tree_height(const binary_tree_t *tree)
-{
-	int lh;
-	int rh;
-
-	if (!tree || (tree->left == NULL && tree->right == NULL))
-		return (0);
-	lh = tree_height(tree->left);
-	rh = tree_height(tree->right);
-
-	printf("left height %d\n right height %d\n", lh, rh);
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
 
 	if (lh >= rh)
 		return (lh + 1);
