657a9e01aa60d143d104b07eb8200233c2ab8e05
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-02 11:06:09 -0400

    betty fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,9 +14,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 			return (binary_tree_is_perfect(tree->left) ==
 			binary_tree_is_perfect(tree->right));
 	}
-	return (0);	
+	return (0);
 }
-
 /**
  * binary_tree_height - measures height of BT
  * @tree: pointer to the root node
