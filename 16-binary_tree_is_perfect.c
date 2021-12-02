2c83224d1c9137ad4b3b3cf5ce1ee88fbb144626
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-02 11:00:47 -0400

    changed edgecase detection

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,7 +13,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_height(tree->left) ==
 	binary_tree_height(tree->right))
 	{
-		return(1);
+		return(binary_tree_is_perfect(tree->left) == binary_tree_is_perfect(tree->right));
 	}
 	return (0);
 }
