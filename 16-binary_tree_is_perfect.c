4d40d52d6796b843ac1fdc2a0bf4d899be59d65c
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-02 10:57:27 -0400

    fixed compilation error

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,10 +10,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		if (binary_tree_is_height(tree->left) ==
-		binary_tree_is_height(tree->right))
+		if (binary_tree_height(tree->left) ==
+		binary_tree_height(tree->right))
 		{
-			return(1)
+			return(1);
 		}
 		return (0);
 	}
