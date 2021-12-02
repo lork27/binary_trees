d25adfd7196d834fa1368cc587f7081be99bd7f9
lork27 <g.lorca.lamadrid@gmail.com>
2021-12-02 11:04:47 -0400

    betty fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,15 +8,13 @@ size_t binary_tree_height(const binary_tree_t *tree);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	if (binary_tree_height(tree->left) ==
-	binary_tree_height(tree->right))
+	if (tree)
 	{
-		return (binary_tree_is_perfect(tree->left) ==
-		binary_tree_is_perfect(tree->right));
+		if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+			return (binary_tree_is_perfect(tree->left) ==
+			binary_tree_is_perfect(tree->right));
 	}
-	return (0);
+	return (0);	
 }
 
 /**
